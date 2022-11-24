868b5e462108dcfa3ae3fe97eb71e9715557bea8
GennaSIL <102751599+GennaSIL@users.noreply.github.com>
2022-11-25 00:33:52 +0200

    Update index.js
    Update the function to catch form input.

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-var firstClick = false;
-var selctedScore;
-
-// rating selector
-
-for (i=0; i<5; i++) {
-    document.querySelectorAll("div.number")[i].addEventListener("click", function() {
-        if (firstClick === true){
-            document.querySelectorAll("div.number")[selctedScore - 1].classList.remove("selected");
-            this.classList.add("selected");
-            selctedScore = this.innerHTML;
-        }else {
-            firstClick = true;
-            selctedScore = this.innerHTML;
-            this.classList.add("selected");
-        }
-    });
+function getScore (event) {
+    var score = document.querySelector("input[name='score']:checked").value;
+    document.querySelector("p.you-selected-line").innerHTML = "You selected " + score + " out of 5";
+    document.querySelector("div.frame").classList.remove("zindex");
 }
-
-// submit button
-
-document.getElementsByTagName("button")[0].addEventListener("click", function() {
-    if (firstClick === false){
-        alert("Please select a rating to submit");
-    } else {
-        document.querySelectorAll("div.frame")[0].classList.remove("zindex");
-        document.querySelectorAll("p.you-selected-line")[0].innerHTML= "You selected " + selctedScore + " out of 5";
-    }
-});
